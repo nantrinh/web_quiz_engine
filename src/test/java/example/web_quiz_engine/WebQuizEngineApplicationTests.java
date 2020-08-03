0a5acc7a1db16e188f4aefbfa3ef43bf258737e6
Nancy Trinh <17187984+nantrinh@users.noreply.github.com>
2020-08-02 17:21:47 -0700

    Add starter code

---
+++ src/test/java/example/web_quiz_engine/WebQuizEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package example.web_quiz_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQuizEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
